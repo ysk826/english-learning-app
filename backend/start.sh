9cdb1470824359eb8b72e9a01e316dd7b1f79a82
ysk826 <ysuke826@gmail.com>
2025-04-20 22:57:39 +0900

    add start.sh and update backend/dockerfile 2nd time

--- backend/start.sh
+++ backend/start.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 # PostgreSQLを起動
-su - postgres -c "pg_ctl -D /var/lib/postgresql/data -l /var/log/postgresql/postgresql.log start"
+su - postgres -c "pg_ctl -D /var/lib/postgresql/data -l /tmp/postgresql.log start"
+
+# 少し待機してPostgreSQLが起動するのを待つ
+sleep 5
 
 # データベースの作成（初回のみ）
 su - postgres -c "createdb -E UTF8 english_app || true"
+
+# アプリケーション環境変数を設定
+export DB_HOST=localhost
+export DB_PORT=5432
+export DB_USER=postgres
+export DB_PASSWORD=postgres
+export DB_NAME=english_app
+export DB_SSL_MODE=disable
+export PORT=10000
+export GIN_MODE=release
 
 # アプリケーションを起動
 /app/main
